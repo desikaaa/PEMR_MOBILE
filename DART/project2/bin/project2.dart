06d1191204f570d9861991892f3a688725934f23
desikaaa <desikarmila211@gmail.com>
2025-11-21 11:39:35 +0700

    update

--- DART/project2/bin/project2.dart
+++ DART/project2/bin/project2.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${project2.calculate()}!');
+  // print('Hello world: ${pertemuan3.calculate()}!');
   // int nilai = 75;
-  stdout.write("Masukkan nilai = ");
+  stdout.write("Masukkan nilai=");
   String? input = stdin.readLineSync();
   int nilai = int.tryParse(input ?? '') ?? 0;
 
@@ -13,18 +13,34 @@ void main(List<String> arguments) {
   // int nilai = int.tryParse(tmp) ?? 0;
 
   // String tmp;
-  // if(input == null){
-  //  tmp = '';
+  // if (input == null) {
+  //   tmp = '';
   // } else {
-  //  tmp = input;
+  //   tmp = input;
   // }
-  // int nilai=int.tryParse(tmp) ?? 0;
+  // int nilai = int.tryParse(tmp) ?? 0;
 
-  if (nilai >= 75) {
+  if ((nilai >= 75) && (nilai <= 100)) {
     print("Nilai A");
-  } else if (nilai >= 60) {
+  } else if ((nilai >= 60) && (nilai <= 74)) {
     print("Nilai B");
-  } else {
+  } else if ((nilai >= 0) && (nilai <= 59)) {
     print("Nilai adalah C");
+  } else {
+    print("Maaf, nilai tidak benar!!");
   }
+
+  // if tradisional
+  String status;
+  if (nilai >= 60) {
+    status = "Lulus";
+  } else {
+    status = "Tidak Lulus";
+  }
+  print(status);
+  //ternary operator
+  String status1 = (nilai >= 60)
+      ? "Alhamdulillah Lulus"
+      : "Maaf anda belum lulus";
+  print(status1);
 }
--- FLUTTER PROJECT/project12/test/widget_test.dart
+++ FLUTTER PROJECT/project12/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:liriklagu/main.dart';
+import 'package:project12/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DesikaApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
